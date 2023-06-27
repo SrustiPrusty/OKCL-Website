de2761c6dcb9e33674e654c52e7c33938cab6360
Srusti Prusty <srustiprusty@gmail.com>
2023-06-27 14:46:55 +0530

    vision

--- visionandmission.js
+++ visionandmission.js
@@ -7,23 +7,23 @@ var btnn2 = document.getElementById("btnn2")
 
 function openVISION() {
     contentt1.style.transform = "translateX(0)"
-    contentt2.style.transform = "translateX(100%)"
+    contentt2.style.transform = "translateX(150%)"
 
     btnn1.style.color = "#ff7846"
     btnn2.style.color = "#001D45"
   
-    contentt1.style.transitionDelay = "0.3s";
+    contentt1.style.transitionDelay = "0.5s";
     contentt2.style.transitionDelay = "0s";
 }
   
 function openMISSION() {
-    contentt1.style.transform = "translateX(100%)"
+    contentt1.style.transform = "translateX(150%)"
     contentt2.style.transform = "translateX(0)"
     
     btnn1.style.color = "#001D45"
     btnn2.style.color = "#ff7846"
     
     contentt1.style.transitionDelay = "0s";
-    contentt2.style.transitionDelay = "0.3s";
+    contentt2.style.transitionDelay = "0.5s";
 
 }
