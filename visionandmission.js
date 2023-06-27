82af6a1abd005e516b8d632ba0dc7101acff5cef
Srusti Prusty <srustiprusty@gmail.com>
2023-06-27 13:04:17 +0530

    v&m

---
+++ visionandmission.js
@@ -0,0 +1,29 @@
+var contentt1 = document.getElementById("contentt1")
+var contentt2 = document.getElementById("contentt2")
+
+var btnn1 = document.getElementById("btnn1")
+var btnn2 = document.getElementById("btnn2")
+
+
+function openVISION() {
+    contentt1.style.transform = "translateX(0)"
+    contentt2.style.transform = "translateX(100%)"
+
+    btnn1.style.color = "#ff7846"
+    btnn2.style.color = "#001D45"
+  
+    contentt1.style.transitionDelay = "0.3s";
+    contentt2.style.transitionDelay = "0s";
+}
+  
+function openMISSION() {
+    contentt1.style.transform = "translateX(100%)"
+    contentt2.style.transform = "translateX(0)"
+    
+    btnn1.style.color = "#001D45"
+    btnn2.style.color = "#ff7846"
+    
+    contentt1.style.transitionDelay = "0s";
+    contentt2.style.transitionDelay = "0.3s";
+
+}
